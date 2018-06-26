8e967bd7afb1ef20d9241bc83fc1659039ab51b6
DJweezy1 <linweezy93@gmail.com>
2018-06-26 15:21:25 -0400

    we dem boyz

---
+++ src/net/bradysaunders/Main.java
@@ -0,0 +1,22 @@
+package net.bradysaunders;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        {
+            System.out.println();
+            System.out.println();
+        }
+        String  = scanner.nextLine();
+
+    public Main(); {
+    }
+    scanner.close();}
+
+    private final  (println)
+
+    {
+        Object println;
+        println
